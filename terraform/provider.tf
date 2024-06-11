4c0426d889518d8650afd8635272b3e6686059e7
SalllesAndr <salllesandr@gmail.com>
2024-06-11 18:47:05 -0300

    primeiro commit

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -14,7 +14,5 @@ terraform {
 
 provider "aws" {
   region                   = "us-east-1"
-  shared_config_files      = [".aws/config"]
-  shared_credentials_files = [".aws/credentials"]
-  profile                  = "iac"
+
 }
